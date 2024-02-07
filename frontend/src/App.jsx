1268b9dd18fb403019495b8fdca0677fffa2df25
Ferid Kamoua <kamouaferid@gmail.com>
2024-02-07 08:03:52 -0500

    original did not run on my pc, fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import GameComponent from './components/GameComponent'; // Import the game component
+import React from "react";
+import GameComponent from "./components/GameComponent"; // Import the game component
 
 function App() {
-    return (
-        <div className="App">
-            <GameComponent />
-        </div>
-    );
+  return (
+    <div className="App">
+      <GameComponent />
+    </div>
+  );
 }
 
 export default App;
