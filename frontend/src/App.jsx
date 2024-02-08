732c318197ec48be48d9d0f708df23829131e440
Shiny Melhiraj <shinymelhiraj@gmail.com>
2024-02-08 17:44:49 -0500

    merge with remote changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import './App.css'
-import InGamePage from './components/in_game_page'
-
-
-function App() {
-  const [count, setCount] = useState(0)
-
-
-
-
-
-  return (
-    <>
-    <InGamePage/>
-    </>
-  )
-=======
 import React from "react";
 import GameComponent from "./components/GameComponent"; // Import the game component
+import InGamePage from './components/in_game_page';
+import HomePage from './components/HomePage'; // Import the home page component
 
 function App() {
   return (
     <div className="App">
-      <GameComponent />
+      <HomePage />
     </div>
   );
->>>>>>> 1268b9dd18fb403019495b8fdca0677fffa2df25
 }
 
 export default App;
